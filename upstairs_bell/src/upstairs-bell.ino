7bb59b6aa80e60080449ce95a5f7a8bf2cefab4f
misterdanb <danb@hasi.it>
2016-06-07 21:19:48 +0200

    Make project platformio compatible

---
+++ upstairs_bell/src/upstairs-bell.ino
@@ -0,0 +1,66 @@
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <Espanol.h>
+
+#define DEBOUNCE_LOW 50
+#define DEBOUNCE_HIGH 500
+
+const String ssid     = "HaSi-Kein-Internet-Legacy";
+const String password = "bugsbunny";
+const String broker   = "atlas";
+int port              = 1883;
+
+int pin = 2;
+int state = HIGH;
+int bounce_counter = 0;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    pinMode(pin, INPUT_PULLUP);
+
+    Espanol.setDebug(true);
+    Espanol.begin(ssid, password, "espalimpalim", broker, port);
+}
+
+void loop()
+{
+    Espanol.loop();
+
+    int current = digitalRead(pin);
+
+    if (current != state)
+    {
+        if (bounce_counter == 1)
+        {
+            if (current == LOW)
+            {
+                Espanol.publish("hasi/door/upstairs/bell", "on");
+            }
+            else
+            {
+                Espanol.publish("hasi/door/upstairs/bell", "off");
+            }
+
+            state = current;
+        }
+
+        if (bounce_counter == 0)
+        {
+            if (current == LOW)
+            {
+                bounce_counter = DEBOUNCE_LOW;
+            }
+
+            if (current == HIGH)
+            {
+                bounce_counter = DEBOUNCE_HIGH;
+            }
+        }
+        else
+        {
+            bounce_counter--;
+        }
+    }
+}
